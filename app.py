47e625bd185839d22ebffb2163e4d62d1f51f731
franterminator <franterminator@hotmail.com>
2018-11-06 14:35:39 +0100

    added versions function

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from telegram import MessageEntity
 from telegram.ext import BaseFilter
 from telegram.ext import CommandHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import InlineQueryHandler
+from telegram.ext import InlineQueryHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 
 
@@ -24,12 +25,18 @@ class FilterTwitch(BaseFilter):
     def filter(self, message):
         return 'twitch.tv' in message.text
 
+class FilterAlguienPara(BaseFilter):
+	def filter(self, message):
+		if('alguien para' in message.text):
+			return True
+		if('Alguien para' in message.text):
+			return True
+		return False
+
 
 
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
-
 
 
 def echo(bot, update):
@@ -49,6 +56,25 @@ def echo(bot, update):
 		bot.delete_message(chat_id=chat_id, message_id=update.message.message_id)
 
 
+def alguien_para_function(bot,update):
+	print(update.message.chat)
+
+	chat_id = update.message.chat_id
+	usuario = update.message.from_user
+	text_from_user = update.message.text
+
+	button_list = [[InlineKeyboardButton("Me apunto",callback_data='bt1')]]
+	reply_markup = InlineKeyboardMarkup(button_list)
+	bot.send_message(chat_id=chat_id, text=text_from_user, reply_markup=reply_markup)
+
+
+def alguien_para_menu_function(bot,update):
+	print(update.callback_query)
+	chat_id = update.callback_query.message.chat.id
+	button_name = update.callback_query.data
+
+	bot.send_message(chat_id=chat_id, text=button_name)
+
 
 
 def fotosexy(bot, update):
@@ -56,21 +82,11 @@ def fotosexy(bot, update):
 	foto = open_images()
 	bot.send_photo(chat_id, foto, caption="mira que sexy soy")
 
+def version(bot,update):
+	chat_id = update.message.chat_id
+	version_text = 'version 1.1'
+	bot.send_message(chat_id=chat_id,text=version_text)
 
-
-def inline_caps(bot, update):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    bot.answer_inline_query(update.inline_query.id, results)
 
 
 
@@ -115,8 +131,13 @@ foto_handler = CommandHandler('foto', fotosexy, pass_args=False)
 dispatcher.add_handler(sexy_handler)
 dispatcher.add_handler(foto_handler)
 
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
+alguien_para_filter = FilterAlguienPara()
+alguien_para_handler = MessageHandler(alguien_para_filter, alguien_para_function)
+dispatcher.add_handler(alguien_para_handler)
+alguien_para_menu_handler = CallbackQueryHandler(alguien_para_menu_function)
+dispatcher.add_handler(alguien_para_menu_handler)
 
+version_handler = CommandHandler('version',version,pass_args=False)
+dispatcher.add_handler(version_handler)
 
 updater.start_polling()
